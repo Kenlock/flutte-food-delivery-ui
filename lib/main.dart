7815a37133a4a723dcbfc9c14bd0ccd802d2c310
KEVIN OTIENO <colvinbrown201@gmail.com>
2021-08-20 10:44:23 +0300

    Final Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Food Delivery UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.grey[50],
+        primaryColor: Colors.deepOrangeAccent
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
